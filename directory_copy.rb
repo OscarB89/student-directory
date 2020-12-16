8e62361851a76304508966d393da67652db24c64
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2020-12-16 13:55:51 +0000

    Q2 Step8 Students whose name begins with a specific letter

--- directory_copy.rb
+++ directory_copy.rb
@@ -22,8 +22,10 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student|
+    if student[:name].start_with?("D") == true
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
