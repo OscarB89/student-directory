60bc4effbe627d51c8f6e5b1522438d1bc29f4b1
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2020-12-17 16:32:43 +0000

    Q7 Step8 Added user input for cohort and checking for typos

--- directory_copy.rb
+++ directory_copy.rb
@@ -1,22 +1,30 @@
 def input_students
-  puts "Please enter the names of the students".center(50)
-  puts "To finish, just hit return twice".center(50)
+  puts "Please enter the names of the students, the cohort'months and the hobbies".center(50)
+  puts "To finish, just hit return 3 times".center(50)
   # create an empty array
   students = []
-  # get the first name
-  name = gets.chomp
-  puts "Please enter the hobby of your students".center(50)
-  puts "To finish, just hit return twice".center(50)
-  # get the first hobby
-  hobby = gets.chomp
+  # month_arr = ["january", "february", "march", "avril", "may", "june", "july", "august", "september", "october", "november", "december"]
+  months = [:january, :february, :march, :avril, :may, :june, :july, :august, :september, :october, :november, :december]
+  # get the first name and month
+  name = gets.chomp.to_sym
+  cohort = gets.chomp.to_sym
+  hobby = gets.chomp.to_sym
+  # January default month if month variable is empty
+  # cohort = "january" if cohort.empty
   # while the name and hobby is not empty
   while !name.empty? && !hobby.empty? do
+  # while month is inappropriate
+    while months.include?(cohort) == false
+      puts "Please enter an appropriate month"
+      cohort = gets.chomp.to_sym
+    end
     # add the student hash to the array
-    students << {name: name, hobby: hobby, cohort: :november}
+    students << {name: name, cohort: cohort, hobby: hobby}
     puts "Now we have #{students.count} students".center(50)
     # get another name from the user
-    name = gets.chomp
-    hobby = gets.chomp
+    name = gets.chomp.to_sym
+    cohort = gets.chomp.to_sym
+    hobby = gets.chomp.to_sym
   end
   students
 end
@@ -29,7 +37,7 @@ end
 def print(students)
   x = 0
   while x < students.count do
-    puts "#{students[x][:name]}, with hobby: #{students[x][:hobby]} (#{students[x][:cohort]} cohort)".center(50)
+    puts "#{students[x][:name]} (#{students[x][:cohort]} cohort) with hobby: #{students[x][:hobby]}".center(50)
     x += 1
   end
 end
