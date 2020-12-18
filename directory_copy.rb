34c823c6ea59276987277e5c3e8cfef44acdde1d
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2020-12-18 15:27:57 +0000

    Q12 Step8 Print if at least one student

--- directory_copy.rb
+++ directory_copy.rb
@@ -3,14 +3,14 @@ def input_students
   puts "To finish, just hit return 3 times".center(50)
   # create an empty array
   students = []
-  # month_arr = ["january", "february", "march", "avril", "may", "june", "july", "august", "september", "october", "november", "december"]
+  # array of months symbols
   months = [:january, :february, :march, :avril, :may, :june, :july, :august, :september, :october, :november, :december]
-  # get the first name and month
+  # get the first name/cohort/hobby and month
   name = gets.strip.to_sym
   cohort = gets.strip.to_sym
   hobby = gets.strip.to_sym
   # January default month if month variable is empty
-  # cohort = "january" if cohort.empty
+  # cohort = "january" if cohort.empty?
   # while the name and hobby is not empty
   while !name.empty? && !hobby.empty? do
   # while month is inappropriate
@@ -25,7 +25,7 @@ def input_students
     else
       puts "Now we have #{students.count} students".center(50)
     end
-    # get another name from the user
+    # get another name/cohort/hobby from the user
     name = gets.strip.to_sym
     cohort = gets.strip.to_sym
     hobby = gets.strip.to_sym
@@ -41,25 +41,21 @@ end
 def print(students)
   puts "What month do you want to display?".center(50)
   user_input = gets.strip.to_sym
-
   sorted_by_cohort = {}
-
   students.each do |new_students|
     cohort = new_students[:cohort]
     students = new_students[:name]
-
     if sorted_by_cohort[cohort] == nil
       sorted_by_cohort[cohort] = [students]
     else
       sorted_by_cohort[cohort].push(students)
     end
   end
-  puts sorted_by_cohort[user_input]
-  # x = 0
-  # while x < students.count do
-  #   puts "#{students[x][:name]} (#{students[x][:cohort]} cohort) with hobby: #{students[x][:hobby]}".center(50)
-  #   x += 1
-  # end
+  if students.empty? == false
+    puts sorted_by_cohort[user_input]
+  else
+    puts "No student in the Villain Academy".center(50)
+  end
 end
 
 def print_footer(students)
