1875628158b126e8ee3fc2009a4ecfc513d98333
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2020-12-19 14:11:19 +0000

    Q5 Step14 Asking for the filename if user chooses items 3 or 4

--- directory_more_exercises.rb
+++ directory_more_exercises.rb
@@ -3,8 +3,8 @@
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "3. Save the list to a file"
+  puts "4. Load the list from a file"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -24,13 +24,18 @@ def process(selection)
     puts "You have successfully selected option 2"
     show_students
   when "9"
+    puts 'You have successfully selected option 9, Bye for now!'
     exit # this will cause the program to terminate
   when "3"
-    puts "You have successfully selected option 3 and saved the students"
-    save_students
+    puts "You have successfully selected option 3 and saved the students."
+    puts 'Please enter the name of the file you would like to save'
+    filename = STDIN.gets.chomp
+    save_students(filename)
   when "4"
     puts "You have successfully selected option 4 and loaded the students from the file"
-    load_students
+    puts "Please enter the name of the file you would like to load"
+    filename = STDIN.gets.chomp
+    load_students(filename)
   else
     puts "I don't know what you meant, try again"
   end
@@ -76,9 +81,9 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
-def save_students
+def save_students(filename)
   # open the file for writing
-  file = File.open("students.csv", "w")
+  file = File.open(filename, "w")
   # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
@@ -88,7 +93,10 @@ def save_students
   file.close
 end
 
-def load_students(filename = "students.csv")
+def load_students(filename)
+  if !File.exists?(filename) == true
+    filename = ("students.csv")
+  end
   file = File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
@@ -98,8 +106,7 @@ def load_students(filename = "students.csv")
 end
 
 def try_load_students
-  # filename = ARGV.first # first argument from the command line
-  filename = "students.csv"
+  filename = ARGV.first# first argument from the command line
   return if filename.nil? # get out of the method if it isn't given
   if File.exists?(filename) # if it exists
     load_students(filename)
---
+++ yippy.rb
@@ -0,0 +1,8 @@
+Qwertt,november
+Qazws,november
+Polnj,november
+ross,november
+o,november
+e,november
+w,november
+3,november
